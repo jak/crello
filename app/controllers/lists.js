336baaea0c41af5c1abe59792156ec3a19a91ecb
Andy Brown <brownie3003@gmail.com>
2014-06-12 11:01:57 +0100

    trying to move a card to a different list

--- app/controllers/lists.js
+++ app/controllers/lists.js
@@ -1,1 +1,13 @@
-lists.js
+var ListsController = Ember.ArrayController.extend({
+    actions: {
+        moveCard: function(card) {
+            var newListNumber = prompt("hello, which list do you want to move " + card.get('title') + " card to?");
+            this.store.find('list', newListNumber).then(function(list) {
+                list.set('cards', card)
+            });
+        }
+
+    }
+});
+
+export default ListsController;
--- app/models/card.js
+++ app/models/card.js
@@ -7,23 +7,19 @@ Card.reopenClass({
   FIXTURES: [
     {
       id: 1,
-      title: "Clone Trello in Ember",
-      list: 1
+      title: "Clone Trello in Ember"
     },
     {
       id: 2,
-      title: "Ship it",
-      list: 1
+      title: "Ship it"
     },
     {
       id: 3,
-      title: "Get rich",
-      list: 1
+      title: "Get rich"
     },
     {
       id: 4,
-      title: "Do some proper work",
-      list: 1
+      title: "Do some proper work"
     }
   ]
 });
