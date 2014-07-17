6e480d35038f528086d247dc0fb60f3a7eb8ebec
andyb <andyb@moo.com>
2014-07-17 14:50:40 +0100

    refactor

--- app/controllers/lists.js
+++ app/controllers/lists.js
@@ -2,11 +2,8 @@ var ListsController = Ember.ArrayController.extend({
     actions: {
         moveCard: function(card) {
             var newListNumber = prompt("hello, which list do you want to move " + card.get('title') + " card to?");
-            var self = this;
             this.store.find('list', newListNumber).then(function(list) {
-                self.store.find('list', card.get('list').id).then(function(oldList) {
-                  oldList.get('cards').removeObject(card);
-                });
+                card.get('list').get('cards').removeObject(card);
                 list.get('cards').pushObject(card);
             });
         }
