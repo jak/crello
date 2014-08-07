1ccbae1b642943c9b249fe86af9a2a3211b69229
Jak Spalding <jak@moo.com>
2014-08-07 10:48:56 +0100

    Adding new cards

--- app/controllers/lists.js
+++ app/controllers/lists.js
@@ -1,11 +1,20 @@
 var ListsController = Ember.ArrayController.extend({
     actions: {
         moveCard: function(card) {
-            var newListNumber = prompt("hello, which list do you want to move " + card.get('title') + " card to?");
+            var newListNumber = window.prompt("hello, which list do you want to move " + card.get('title') + " card to?");
             this.store.find('list', newListNumber).then(function(list) {
                 card.get('list').get('cards').removeObject(card);
                 list.get('cards').pushObject(card);
             });
+        },
+        createCard: function (list) {
+          var title = window.prompt('Tell me something special');
+
+          var card = this.store.createRecord('card', {
+            title: title
+          });
+          card.save();
+          list.get('cards').pushObject(card);
         }
     }
 });
